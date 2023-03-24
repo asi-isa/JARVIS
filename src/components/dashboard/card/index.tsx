f270863121a0b7216822805c88e0f98cb71dcdc4
asi-isa <ali.inceoglu@protonmail.com>
2023-03-24 08:42:00 +0100

    dashboard main header ui

---
+++ src/components/dashboard/card/index.tsx
@@ -0,0 +1,7 @@
+interface CardProps {}
+
+const Card = ({}: CardProps) => {
+  return <div></div>;
+};
+
+export default Card;
--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,8 +1,6 @@
-import Brand from "../Brand";
-import Divider from "../Divider";
-import MenuItems from "./menu/MenuItems";
-import ThemeToggle from "../ThemeToggle";
 import Menu from "./menu";
+
+import { AiOutlineSearch } from "react-icons/ai";
 
 interface DashboardProps {}
 
@@ -12,7 +10,29 @@ const Dashboard = ({}: DashboardProps) => {
       <Menu />
 
       {/* MAIN */}
-      <div></div>
+      <div className="flex flex-col gap-4 w-full px-8">
+        {/* HEADER */}
+        <div className="flex justify-between items-center">
+          {/* TITLE */}
+          <p className="text-2xl text-[var(--white)]">Dashboard</p>
+
+          <div className="flex items-center gap-2">
+            <AiOutlineSearch className="text-[var(--white)] text-xl" />
+            <p className="text-sm">Search Something...</p>
+          </div>
+
+          <p className="font-semibold  text-[var(--black)] bg-[var(--accent)] py-2 px-4 rounded-xl">
+            Upgrade
+          </p>
+        </div>
+
+        {/* BODY */}
+        <div className="h-full">
+          <p>flex grid, two rows three cols</p>
+          {/* <Card row={1} col={1} rowSpan={1} colSpan={1} /> */}
+          {/* <Card row={1} col={2} rowSpan={2} colSpan={1} /> */}
+        </div>
+      </div>
     </div>
   );
 };
